2012d7adb5d49189e202b97e5294faaa0400717a
Richard Day <r.day80@gmail.com>
2015-07-21 22:00:41 +0100

    Significantly improve StrToDate() to allow it to handle a multitude of formats with both relative and absolute specifications.

--- src/DateTime.h
+++ src/DateTime.h
@@ -6,24 +6,34 @@
 
 #include "misc.h"
 #include "strsup.h"
+#include "DataList.h"
 
 #define SECONDS_PER_DAY (24 * 3600)
 #define MS_PER_DAY (SECONDS_PER_DAY * 1000)
 
 class ADateTime {
 public:
+	enum {
+		Time_Absolute = 0,
+		Time_Existing,
+		Time_Relative_Today,
+		Time_Relative_Today_UTC,
+		Time_Relative_UTC,
+		Time_Relative_Local,
+	};
+
 	ADateTime();
 	ADateTime(uint32_t days, uint32_t ms);
 	ADateTime(uint64_t ms);
 	ADateTime(const ADateTime& object);
-	ADateTime(const AString&   str);
+	ADateTime(const AString&   str, uint_t relative = Time_Relative_Today, uint_t *specified = NULL);
 
 	static const uint32_t DaysSince1970;
 #ifdef _WIN32
 	static const uint32_t DaysSince1601;
 #endif
 
-	ADateTime& Set(uint8_t day, uint8_t month, uint16_t year, uint8_t hr = 0, uint8_t minute = 0, uint8_t second = 0);
+	ADateTime& Set(uint_t day, uint_t month, uint_t year, uint_t hr = 0, uint_t minute = 0, uint_t second = 0);
 	ADateTime& Set(uint32_t days, uint32_t ms) {DateTime.MilliSeconds = ms; DateTime.Days = days; return *this;}
 	ADateTime& Set(uint64_t ms);
 	ADateTime& SetDays(uint32_t days) {DateTime.Days         = days; return *this;}
@@ -35,21 +45,21 @@ public:
 	ADateTime& NextWeek() {return Set(GetDays() + 7, GetMS());}
 	ADateTime& PrevWeek() {return Set(GetDays() - 7, GetMS());}
 
-	ADateTime& PrevWeekDay(uint8_t wd) {assert(wd < 7); uint_t d = (GetWeekDay() + 7 - wd) % 7; return Set(GetDays() - d, GetMS());}
-	ADateTime& NextWeekDay(uint8_t wd) {assert(wd < 7); uint_t d = (wd + 7 - GetWeekDay()) % 7; return Set(GetDays() + d, GetMS());}
+	ADateTime& PrevWeekDay(uint_t wd) {assert(wd < 7); uint_t d = (GetWeekDay() + 7 - wd) % 7; return Set(GetDays() - d, GetMS());}
+	ADateTime& NextWeekDay(uint_t wd) {assert(wd < 7); uint_t d = (wd + 7 - GetWeekDay()) % 7; return Set(GetDays() + d, GetMS());}
 
-	uint32_t GetDays()  const {return DateTime.Days;}
-	uint32_t GetMS()    const {return DateTime.MilliSeconds;}
+	uint32_t GetDays()    const {return DateTime.Days;}
+	uint32_t GetMS()      const {return DateTime.MilliSeconds;}
 	
-	uint8_t  GetWeekDay() const;
-	uint8_t  GetDay()     const;
-	uint8_t  GetMonth()   const;
-	uint16_t GetYear()    const;
+	uint_t   GetWeekDay() const;
+	uint_t   GetDay()     const;
+	uint_t   GetMonth()   const;
+	uint_t   GetYear()    const;
 
-	uint8_t  GetHours()   const;
-	uint8_t  GetMinutes() const;
-	uint8_t  GetSeconds() const;
-	uint16_t GetMilliSeconds() const;
+	uint_t   GetHours()   const;
+	uint_t   GetMinutes() const;
+	uint_t   GetSeconds() const;
+	uint_t   GetMilliSeconds() const;
 
 	uint64_t GetAbsoluteSecond() const {return (uint64_t)DateTime.Days * SECONDS_PER_DAY + DateTime.MilliSeconds / 1000;}
 	uint32_t GetAbsoluteHour() 	 const {return DateTime.Days * 24 + GetHours();}
@@ -89,7 +99,7 @@ public:
 		Specified_Date = 2,
 		Specified_Day  = 4,
 	};
-	ADateTime& StrToDate(const AString& String, bool current = true, uint_t *specified = NULL);
+	ADateTime& StrToDate(const AString& String, uint_t relative = Time_Relative_Today, uint_t *specified = NULL);
 
 	operator uint64_t() const;
 	operator sint64_t() const;
@@ -102,10 +112,11 @@ public:
 	uint_t GetMonthStartDay(uint_t m) const;
 
 	time_t     totime() const;
-	time_t	   totime_local() const;
 	ADateTime& fromtime(time_t t);
 	ADateTime& fromtime(const struct tm *tm);
 	ADateTime& fromtime_local(time_t t);
+
+	sint64_t   GetUTCOffset() const;
 
 	ADateTime  UTCToLocal() const;
 	ADateTime  LocalToUTC() const;
@@ -120,9 +131,6 @@ public:
 	AString ToTimeStamp(uint_t format = TIMESTAMP_FORMAT_TERSE) const;
 	bool    FromTimeStamp(const AString& str, bool utc = false);
 
-	static sint32_t  GetLocalOffsetMinutes();
-	static ADateTime GetLocalOffset(bool& backwards);
-
 	static const ADateTime MinDateTime;
 	static const ADateTime MaxDateTime;
 
@@ -132,15 +140,45 @@ protected:
 		uint32_t Days;
 	} DATETIME;
 	typedef struct {
-		uint8_t WeekDay;
-		uint8_t Day;
-		uint8_t Month;
-		uint16_t Year;
+		uint_t WeekDay;
+		uint_t Day;
+		uint_t Month;
+		uint_t Year;
 	} DAYMONTHYEAR;
+
+	typedef struct {
+		AValue val;
+		AString str;
+		AString numstr;
+	} TERM;
+	static void __DeleteTerm(uptr_t item, void *context) {
+		(void)context;
+		delete (TERM *)item;
+	}
+	uint_t ParseTerms(const AString& str, ADataList& list) const;
 
 	void InitData();
 
+	uint_t GetMonthLength(const DAYMONTHYEAR& dmy) const {return GetMonthLength(dmy.Month, dmy.Year);}
+	uint_t GetMonthLength(uint_t m, uint_t y) const;
+	uint_t GetMonthStart(const DAYMONTHYEAR& dmy) const {return GetMonthStart(dmy.Month, dmy.Year);}
+	uint_t GetMonthStart(uint_t m, uint_t y) const;
+	uint_t GetMonthEnd(const DAYMONTHYEAR& dmy) const {return GetMonthEnd(dmy.Month, dmy.Year);}
+	uint_t GetMonthEnd(uint_t m, uint_t y) const;
+
 	void CurrentTime(DATETIME& dt, bool utc = false) const;
+
+	void AddDateTime(DATETIME& dt, uint32_t days, uint32_t ms) const;
+	void SubDateTime(DATETIME& dt, uint32_t days, uint32_t ms) const;
+
+	void AddDMY(DAYMONTHYEAR& dmy, uint_t years, uint_t months, uint_t days) const;
+	void SubDMY(DAYMONTHYEAR& dmy, uint_t years, uint_t months, uint_t days) const;
+
+	void ModifyYear(DATETIME& dt, double n, bool pos, bool neg) const;
+	void ModifyMonth(DATETIME& dt, double n, bool pos, bool neg) const;
+	void ModifyDay(DATETIME& dt, double n, bool pos, bool neg) const;
+	void ModifyDays(DATETIME& dt, double n, bool pos, bool neg) const;
+	void ModifyMS(DATETIME& dt, sint64_t n, bool pos, bool neg) const;
 
 	void DateTimeToDMY(const DATETIME& dt, DAYMONTHYEAR& dmy) const;
 	void DMYToDateTime(const DAYMONTHYEAR& dmy, DATETIME& dt) const;
