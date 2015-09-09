cd968b17d530ece9971793779d8f33725df65f6d
Richard Day <r.day80@gmail.com>
2015-09-09 22:46:52 +0100

    Add ASocketServer::Handler class to make writing socket handlers easier. Add HTTPRequest class to allow simple HTTP requests.

---
+++ src/HTTPRequest.h
@@ -0,0 +1,37 @@
+#ifndef __HTTP_REQUEST__
+#define __HTTP_REQUEST__
+
+#include <vector>
+
+#include "SocketServer.h"
+
+class AHTTPRequest : public ASocketServer::Handler {
+public:
+	AHTTPRequest(ASocketServer *_server = NULL);
+	AHTTPRequest(ASocketServer *_server, const AString& _url);
+	virtual ~AHTTPRequest();
+
+	virtual bool Open(const AString& _url);
+	virtual bool IsComplete() const {return complete;}
+	
+protected:
+	virtual void OnConnect();
+	virtual void OnRead();
+	virtual void OnWrite();
+	virtual bool NeedWrite() {return sendrequest;}
+	virtual void Cleanup();
+
+	virtual void ProcessData() {}
+
+	virtual bool GetHeader(AString& header, uint_t maxlen);
+
+protected:
+	AString 		   	 url;
+	AString 		   	 host;
+	uint_t  		   	 port;
+	bool    		   	 sendrequest;
+	bool				 complete;
+	std::vector<uint8_t> data;
+};
+
+#endif
