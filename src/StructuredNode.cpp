8ac8ae7f1e3cd6f24b1a9366fab6787cfa4401ab
Richard Day <r.day80@gmail.com>
2016-01-16 23:23:44 +0000

    Change types of attributes and children within AStructuredNode and add find functions.

---
+++ src/StructuredNode.cpp
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "StructuredNode.h"
+
+NODETYPE_IMPLEMENT(AStructuredNode);
+
+AStructuredNode::AStructuredNode() : AKeyValuePair(),
+									 pAttributes(NULL),
+									 pChildren(NULL),
+									 Type(0)
+{
+}
+
+AStructuredNode::~AStructuredNode()
+{
+	DELETE_LIST(pAttributes);
+	DELETE_LIST(pChildren);
+}
+	
+const AKeyValuePair *AStructuredNode::FindAttribute(const AString& Name, const AKeyValuePair *pAttribute) const
+{
+	if (pAttribute) pAttribute = pAttribute->Next();
+	else			pAttribute = GetAttributes();
+	while (pAttribute && (pAttribute->Key != Name)) pAttribute = pAttribute->Next();
+	return pAttribute;
+}
+
+AString AStructuredNode::GetAttribute(const AString& Name, const AKeyValuePair *pAttribute) const
+{
+	AString res;
+	if ((pAttribute = FindAttribute(Name, pAttribute)) != NULL) res = pAttribute->Value;
+	return res;
+}
+	
+const AStructuredNode *AStructuredNode::FindChild(const AString& name, const AStructuredNode *pChild) const
+{
+	if (pChild) pChild = pChild->Next();
+	else		pChild = GetChildren();
+	while (pChild && (pChild->Key != name)) pChild = pChild->Next();
+	return pChild;
+}
+
+AString AStructuredNode::GetChildValue(const AString& Name, const AStructuredNode *pChild) const
+{
+	AString res;
+	if ((pChild = FindChild(Name, pChild)) != NULL) res = pChild->Value;
+	return res;
+}
+
+void AStructuredNode::ToString(AString& str, uint_t level) const
+{
+	const AKeyValuePair   *pAttribute = pAttributes;
+	const AStructuredNode *pChild     = pChildren;
+	const AString         header      = AString("  ").Copies(level);
+	
+	str.printf("%sNode '%s' value '%s'", header.str(), Key.str(), Value.str());
+	if (pAttribute) {
+		bool first = true;
+		
+		str.printf(" (");
+		while (pAttribute) {
+			if (!first) str.printf(", ");
+			first = false;
+
+			str.printf("%s='%s'", pAttribute->Key.str(), pAttribute->Value.str());
+
+			pAttribute = pAttribute->Next();
+		}
+
+		str.printf(")");
+	}
+	str.printf("\n");
+
+	while (pChild) {
+		pChild->ToString(str, level + 1);
+		
+		pChild = pChild->Next();
+	}
+}
--- src/StructuredNode.h
+++ src/StructuredNode.h
@@ -5,27 +5,31 @@
 
 class AStructuredNode : public AKeyValuePair {
 public:
-	AStructuredNode() : AKeyValuePair(),
-						pAttributes(NULL),
-						pChildren(NULL),
-						Type(0) {}
-	virtual ~AStructuredNode() {DELETE_LIST(pAttributes); DELETE_LIST(pChildren);}
+	AStructuredNode();
+	virtual ~AStructuredNode();
 	
 	LIST_FUNCTIONS_EX(AStructuredNode, AKeyValuePair);
 
 	void    SetType(uint8_t type) {Type = type;}
 	uint8_t GetType()       const {return Type;}
 	
-	const AListNode *GetAttributes()		 const {return pAttributes;}
-	void			AddAttribute(AListNode *pNode) {pAttributes = pNode->Append(pAttributes);}
-		
-	const AListNode *GetChildren()			 const {return pChildren;}
-	void			AddChild(AListNode *pNode)     {pChildren = pNode->Append(pChildren);}
+	const AKeyValuePair *GetAttributes()		     const {return pAttributes;}
+	void				AddAttribute(AKeyValuePair *pNode) {pAttributes = pNode->Append(pAttributes);}
+	const AKeyValuePair *FindAttribute(const AString& Name, const AKeyValuePair *pAttribute = NULL) const;
+	AString GetAttribute(const AString& Name, const AKeyValuePair *pAttribute = NULL) const;
+	
+	const AStructuredNode *GetChildren()		     const {return pChildren;}
+	void				  AddChild(AStructuredNode *pNode) {pChildren = pNode->Append(pChildren);}
+	const AStructuredNode *FindChild(const AString& name, const AStructuredNode *pChild = NULL) const;
+	AString GetChildValue(const AString& Name, const AStructuredNode *pChild = NULL) const;
 
+	void ToString(AString& str, uint_t level = 0) const;
+	AString ToString() const {AString str; ToString(str); return str;}
+	
 protected:
-	AListNode *pAttributes;
-	AListNode *pChildren;
-	uint8_t	  Type;
+	AKeyValuePair   *pAttributes;
+	AStructuredNode *pChildren;
+	uint8_t			Type;
 	
 protected:
 	NODETYPE_DEFINE(AStructuredNode);
