e148225c2ef9ab1ebb7c50dd34f4bc0b46779504
Richard Day <r.day80@gmail.com>
2016-11-16 22:02:03 +0000

    Add -O0 to debug build flags

--- include/rdlib/mt19937ar.h
+++ include/rdlib/mt19937ar.h
@@ -5,6 +5,8 @@
 extern "C" {
 #endif
 
+extern void init_genrand(unsigned long s);
+extern void init_by_array(unsigned long init_key[], int key_length);
 extern unsigned long genrand_int32(void);
 extern long genrand_int31(void);
 extern double genrand_real1(void);
