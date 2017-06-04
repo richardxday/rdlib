d72345c7b505a708435d0cd552bfef18149df7ea
Richard Day <r.day80@gmail.com>
2017-06-04 22:21:13 +0100

    Allow assignment to variables using ':='.  Add a number of math functions.

--- include/rdlib/simpleeval.h
+++ include/rdlib/simpleeval.h
@@ -12,8 +12,9 @@ typedef std::map<AString, AValue> simplevars_t;
 typedef std::vector<AValue>		  simpleargs_t;
 
 extern void AddSimpleFunction(const AString& name, AValue (*fn)(const AString& funcname, const simplevars_t& vars, const simpleargs_t& values, AString& errors, void *context), void *context = NULL);
+extern void RemoveSimpleFunction(const AString& name);
 
-extern uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue& val, AString& errors);
-extern bool Evaluate(const simplevars_t& vars, const AString& expr, AValue& val, AString& errors);
+extern uint_t Evaluate(simplevars_t& vars, const AString& str, uint_t pos, AValue& val, AString& errors);
+extern bool Evaluate(simplevars_t& vars, const AString& expr, AValue& val, AString& errors);
 
 #endif
--- src/simpleeval.cpp
+++ src/simpleeval.cpp
@@ -6,6 +6,8 @@
 
 #include "simpleeval.h"
 
+#define DEBUGEVAL 0
+
 #define IsWhiteSpaceEx(c) (IsWhiteSpace(c) || (c == '\n') || (c == '\r'))
 
 typedef struct {
@@ -19,6 +21,85 @@ void AddSimpleFunction(const AString& name, AValue (*fn)(const AString& funcname
 {
 	funcs[name].fn = fn;
 	funcs[name].context = context;
+}
+
+void RemoveSimpleFunction(const AString& name)
+{
+	std::map<AString, simplefunc_t>::iterator it;
+
+	if ((it = funcs.find(name)) != funcs.end()) {
+		funcs.erase(it);
+	}
+}
+
+static const struct {
+	const char *funcname;
+	size_t nparameters;
+} __math_parameters[] = {
+	{"sin", 1},
+	{"cos", 1},
+	{"tan", 1},
+	{"asin", 1},
+	{"acos", 1},
+	{"atan", 1},
+	{"atan2", 2},
+	{"exp", 1},
+	{"log", 1},
+	{"ln", 1},
+	{"sqrt", 1},
+	{"pow", 2},
+	{"mod", 2},
+	{"deg2rad", 1},
+	{"rad2deg", 1},
+};
+
+static AValue __math(const AString& funcname, const simplevars_t& vars, const simpleargs_t& values, AString& errors, void *context)
+{
+	static std::map<AString,size_t> expectedparameters;
+	std::map<AString,size_t>::const_iterator it;
+	AValue res;
+
+	UNUSED(vars);
+	UNUSED(context);
+	
+	if (expectedparameters.end() == expectedparameters.begin()) {
+		uint_t i;
+
+		for (i = 0; i < NUMBEROF(__math_parameters); i++) {
+			expectedparameters[__math_parameters[i].funcname] = __math_parameters[i].nparameters;
+		}
+	}
+
+	if ((it = expectedparameters.find(funcname)) != expectedparameters.end()) {
+		if (it->second == values.size()) {
+			if		(funcname == "sin")   	res = sin((double)values[0]);
+			else if (funcname == "cos")   	res = cos((double)values[0]);
+			else if (funcname == "tan")   	res = tan((double)values[0]);
+			else if (funcname == "asin")  	res = asin((double)values[0]);
+			else if (funcname == "acos")  	res = acos((double)values[0]);
+			else if (funcname == "atan")  	res = atan((double)values[0]);
+			else if (funcname == "atan2") 	res = atan2((double)values[0], (double)values[1]);
+			else if (funcname == "exp")   	res = exp((double)values[0]);
+			else if (funcname == "log")   	res = log10((double)values[0]);
+			else if (funcname == "ln")    	res = log((double)values[0]);
+			else if (funcname == "sqrt")  	res = sqrt((double)values[0]);
+			else if (funcname == "pow")   	res = pow((double)values[0], (double)values[1]);
+			else if (funcname == "mod")   	res = fmod((double)values[0], (double)values[1]);
+			else if (funcname == "deg2rad") res = (double)values[0] * M_PI / 180.0;
+			else if (funcname == "rad2deg") res = (double)values[0] * 180.0 / M_PI;
+			else {
+				errors.printf("Unimplemented function '%s'\n", funcname.str());
+			}
+		}
+		else {
+			errors.printf("Incorrect number of parameters to '%s', expected %u, supplied %u\n", funcname.str(), (uint_t)it->second, (uint_t)values.size());
+		}
+	}
+	else {
+		errors.printf("Unknown function '%s'\n", funcname.str());
+	}
+	
+	return res;
 }
 
 static uint_t findendquote(const AString& str, uint_t pos, char c)
@@ -165,7 +246,7 @@ static uint_t getoper(const AString& str, uint_t pos, AString& arg)
 	return pos;
 }
 
-uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue& val, AString& errors)
+uint_t Evaluate(simplevars_t& vars, const AString& str, uint_t pos, AValue& val, AString& errors)
 {
 	enum {
 		State_First_PreModifier = 0,
@@ -179,10 +260,23 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 	uint_t  state = State_First_PreModifier;
 	uint_t  operpos = 0;
 
+	if (funcs.find(__math_parameters[0].funcname) == funcs.end()) {
+		uint_t i;
+		
+		for (i = 0; i < NUMBEROF(__math_parameters); i++) {
+			AddSimpleFunction(__math_parameters[i].funcname, &__math);
+		}
+	}
+	
 	while (str[pos]) {
 		pos = skipwhitespace(str, pos);
 
 		if (str[pos] == ')') break;
+		if (str[pos] == ',') {
+			pos++;
+			val1  = 0;
+			state = State_First_PreModifier;
+		}
 
 		uint_t pos0 = pos;
 		switch (state) {
@@ -201,6 +295,9 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 					if (!Evaluate(vars, subexpr, 0, val1, errors)) {
 						errors.printf("Failed to evaluate expression '%s' at '%s'\n", subexpr.str(), str.Mid(pos0).str());
 					}
+#if DEBUGEVAL
+					else debug("Result of '%s' is %s\n", subexpr.str(), val1.ToString().str());
+#endif
 				}
 				else if (IsSymbolStart(arg[0])) {
 					simplevars_t::const_iterator it;
@@ -215,7 +312,7 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 
 					if (args.Valid()) {
 						std::map<AString, simplefunc_t>::const_iterator it;
-						
+
 						if ((it = funcs.find(arg)) == funcs.end()) {
 							errors.printf("Function '%s' not found at '%s'\n", arg.str(), str.Mid(pos0).str());
 						}
@@ -243,10 +340,25 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 						}
 						else errors.printf("Invalid args '%s' for function '%s' at '%s'\n", args.str(), arg.str(), str.Mid(pos0).str());
 					}
+					else if (str.Mid(pos, 2) == ":=") {
+						uint_t pos0 = pos + 2;
+						pos = Evaluate(vars, str, pos0, val1, errors);
+						if (errors.Empty()) {
+#if DEBUGEVAL
+							debug("vars[%s] = %s (from '%s')\n", arg.str(), val1.ToString().str(), str.Mid(pos0, pos - pos0).str());
+#endif
+							vars[arg] = val1;
+						}
+					}
 					else if ((it = vars.find(arg)) == vars.end()) {
 						errors.printf("Variable '%s' not found at '%s'\n", arg.str(), str.Mid(pos0).str());
 					}
-					else val1 = it->second;
+					else {
+						val1 = it->second;
+#if DEBUGEVAL
+						debug("Var '%s' is %s\n", arg.str(), val1.ToString().str());
+#endif
+					}
 				}
 				else {
 					val1 = AValue::EvalNumberEx(arg.str(), true, NULL, &errors);
@@ -261,25 +373,29 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 					state++;
 				}
 				else {
-					if		(oper  == "+")	val	 += val1;
-					else if (oper  == "-")	val	 -= val1;
-					else if (oper  == "*")	val	 *= val1;
-					else if (oper  == "^")	val	 ^= val1;
-					else if (oper  == "&")	val	 &= val1;
-					else if (oper  == "|")	val	 |= val1;
-					else if (oper  == "&&")	val	  = val && val1;
-					else if (oper  == "||")	val	  = val || val1;
-					else if (oper  == "==")	val	  = (val == val1);
-					else if (oper  == "!=")	val	  = (val != val1);
-					else if (oper  == "<=")	val	  = (val <= val1);
-					else if (oper  == ">=")	val	  = (val >= val1);
-					else if (oper  == "<>")	val	  = (val != val1);
-					else if (oper  == "=")	val	  = (val == val1);
-					else if (oper  == "<")	val	  = (val <  val1);
-					else if (oper  == ">")	val	  = (val >  val1);
-					else if (oper  == "<<")	val	<<= val1;
-					else if (oper  == ">>")	val	>>= val1;
-					else if (oper  == "%") {
+#if DEBUGEVAL
+					debug("val=%s, val1=%s, oper='%s'\n", val.ToString().str(), val1.ToString().str(), oper.str());
+#endif
+					
+					if		(oper == "+")  val  += val1;
+					else if (oper == "-")  val  -= val1;
+					else if (oper == "*")  val  *= val1;
+					else if (oper == "^")  val  ^= val1;
+					else if (oper == "&")  val  &= val1;
+					else if (oper == "|")  val  |= val1;
+					else if (oper == "&&") val   = val && val1;
+					else if (oper == "||") val   = val || val1;
+					else if (oper == "==") val   = (val == val1);
+					else if (oper == "!=") val   = (val != val1);
+					else if (oper == "<=") val   = (val <= val1);
+					else if (oper == ">=") val   = (val >= val1);
+					else if (oper == "<>") val   = (val != val1);
+					else if (oper == "=")  val   = (val == val1);
+					else if (oper == "<")  val   = (val <  val1);
+					else if (oper == ">")  val   = (val >  val1);
+					else if (oper == "<<") val <<= val1;
+					else if (oper == ">>") val >>= val1;
+					else if (oper == "%") {
 						if (val1.IsZero()) errors.printf("Modulo by zero at '%s'\n", str.Mid(operpos).str());
 						else			   val = val % val1;
 					}
@@ -294,6 +410,9 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 			case State_Operator:
 				operpos = pos;
 				pos   	= getoper(str, pos, oper);
+				if (oper.Empty()) {
+					errors.printf("Unrecognized operator '%s' at '%s'\n", oper.str(), str.Mid(operpos).str());
+				}
 				state 	= State_Second_PreModifier;
 				break;
 
@@ -310,13 +429,13 @@ uint_t Evaluate(const simplevars_t& vars, const AString& str, uint_t pos, AValue
 	return pos;
 }
 
-bool Evaluate(const simplevars_t& vars, const AString& expr, AValue& val, AString& errors)
+bool Evaluate(simplevars_t& vars, const AString& expr, AValue& val, AString& errors)
 {
 	bool success = true;
 	uint_t pos = 0;
 
 	pos = Evaluate(vars, expr, pos, val, errors);
-	success = (expr[pos] == 0);
+	success = (errors.Empty() && (expr[pos] == 0));
 
 	return success;
 }
