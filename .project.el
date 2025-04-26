0c704d9a32cf9dfbaa3933fc72d3502d7899f3a3
Richard Day <r.day80@gmail.com>
2025-04-26 09:00:42 +0100

    New project file format using yaml.

--- .project.el
+++ .project.el
@@ -1,13 +1,14 @@
-(c++-mode . ((filetypes . (cpp-project))
-             (includes . ("include"
-                          "winlib"
-                          "/usr/include/postgresql"))
-             (defines . ("_FILE_OFFSET_BITS=64"
-                         "__LINUX__"))
-             (flags . ("-xc++"
-                       "-std=c++11"))
-             (warnings . ("all"
-                          "extra"
-                          "everything"))
-             (sources . ("src"))
-             (buildcmd . "make -j8 -f makefile")))
+(c++-mode . ((target . default)
+             (targets . (default . ((filetypes . (cpp-project))
+                                    (includes . ("include"
+                                                 "winlib"
+                                                 "/usr/include/postgresql"))
+                                    (defines . ("_FILE_OFFSET_BITS=64"
+                                                "__LINUX__"))
+                                    (flags . ("-xc++"
+                                              "-std=c++11"))
+                                    (warnings . ("all"
+                                                 "extra"
+                                                 "everything"))
+                                    (sources . ("src"))
+                                    (buildcmd . "make -j8 -f makefile"))))))
